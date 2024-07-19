2346fa500ccda4eb45d1514802a9c9b10be067d2
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-07-19 10:34:04 -0300

    add link  button withIcon props component

---
+++ src/components/LinkButtonWithIcon.tsx
@@ -0,0 +1,15 @@
+import { IconType } from "react-icons";
+
+interface LinkButtonWithIconProps {
+  text: string;
+  icon: IconType;
+  href?: string;
+}
+
+const LinkButtonWithIcon = ({}: LinkButtonWithIconProps) => (
+  <div>
+    <div></div>
+  </div>
+);
+
+export default LinkButtonWithIcon;
