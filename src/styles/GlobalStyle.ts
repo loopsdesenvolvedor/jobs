ee5f823b3cd216190c0835df5b808e795b26a7cf
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-07-23 00:51:14 -0300

    updating globalstyle formatting

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -7,14 +7,18 @@ const GlobalStyle = createGlobalStyle`
         font-weight: 300;
         color: #333;
         box-sizing: border-box;
+
     }
 
     body{
         margin: 0;
         padding: 0;
+    
     }
 
-    *,*::before,*::after{
+    *,
+    *::before,
+    *::after{
         box-sizing: inherit;
     }
 
