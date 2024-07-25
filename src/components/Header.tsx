1185e5de6691d7f74ba5c874141750a16b9fd4ac
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-07-25 01:41:52 -0300

    add menu big screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import { BreakpointsSize, breakAt } from "@/styles/BreakPointsSize";
 import logo from "../../public/jobs.svg";
 
 import { IoMdMenu, IoIosSearch } from "react-icons/io";
-import { FaBell } from "react-icons/fa";
+import { FaBell, FaUserCircle } from "react-icons/fa";
 
 const Root = styled.header`
   width: 100%;
@@ -17,11 +17,20 @@ const Root = styled.header`
 const Navbar = styled.nav`
   width: 100%;
   height: 6rem;
+  margin: 0 auto;
   padding: 0 2rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin: 0 auto;
+
+  ${breakAt(BreakpointsSize.lg)} {
+    padding: 0 5rem;
+    & img {
+      width: 8.6rem;
+      height: 2.8rem;
+    }
+  }
 `;
 
 const ButtonsNavContent = styled.div`
@@ -34,6 +43,71 @@ const ButtonsNavContent = styled.div`
     background-color: transparent;
     border: none;
     padding: 0;
+    cursor: pointer;
+  }
+
+  .login {
+    display: none;
+  }
+
+  ${breakAt(BreakpointsSize.lg)} {
+    display: flex;
+
+    .search {
+      display: none;
+    }
+
+    .menu-toggle,
+    .notification {
+      display: flex;
+      align-items: center;
+      gap: 0.6rem;
+    }
+  }
+
+  .menu-toggle {
+    text-transform: uppercase;
+    font-weight: 500;
+  }
+
+  .notification {
+    border: 0.1rem solid rgba(51, 51, 51, 0.1);
+    padding: 0.5rem 0.9rem;
+    border-radius: 0.4rem;
+    font-size: 1.4rem;
+    transition: all 0.3s ease;
+
+    svg {
+      width: 1.2rem;
+    }
+
+    &:hover {
+      border: 0.1rem solid #264fbc;
+      background-color: #264fbc;
+      color: #fff;
+      svg {
+        transition: all 0.3s ease;
+        fill: #fff;
+      }
+    }
+  }
+
+  .login {
+    display: flex;
+    align-items: center;
+    gap: 0.6rem;
+    border: 0.1rem solid #264fbc;
+    background-color: #264fbc;
+    padding: 0.5rem 0.9rem;
+    border-radius: 0.4rem;
+    font-size: 1.4rem;
+    color: #fff;
+    text-decoration: none;
+    transition: all 0.3s ease;
+
+    &:hover {
+      opacity: 0.8;
+    }
   }
 `;
 
@@ -45,14 +119,21 @@ const Header = () => (
       </Link>
 
       <ButtonsNavContent>
-        <button>
+        <button className="search">
           <IoIosSearch size={22} color="#264FBC" />
         </button>
-        <button>
+
+        <button className="notification">
           <FaBell size={20} color="#264FBC" />
+          <span>Alertas</span>
         </button>
-        <button>
+        <Link className="login" href="/">
+          <FaUserCircle size={12} color="#fff" />
+          <span>Login</span>
+        </Link>
+        <button className="menu-toggle">
           <IoMdMenu size={30} color="#264FBC" />
+          <span>Menu</span>
         </button>
       </ButtonsNavContent>
     </Navbar>
