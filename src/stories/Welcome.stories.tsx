6869bb13af6d276251f9fefa4ac5d4d9bcbde3ad
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-09-26 19:11:43 -0300

    add initial config storybook

---
+++ src/stories/Welcome.stories.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+export default {
+  title: "Welcome",
+};
+
+export const toJobs = () => (
+  <div>
+    <h1>Jobs</h1>
+    <p>
+      Jobs é uma plataforma moderna e intuitiva que conecta candidatos a
+      oportunidades de emprego. O site permite que os usuários se cadastrem,
+      publiquem vagas de emprego e explorem uma variedade de oportunidades em
+      diferentes setores.
+    </p>
+    <h2>Para Candidatos:</h2>
+    <ul>
+      <li>
+        <strong>Cadastro Fácil: </strong>
+        Crie uma conta em poucos minutos e comece a explorar as vagas
+        disponíveis.
+      </li>
+      <li>
+        <strong>Busca Personalizada:</strong>
+        Utilize filtros para encontrar empregos que correspondam ao seu perfil,
+        localização e preferências.
+      </li>
+      <li>
+        <strong> Aplicação Rápida:</strong>
+        Candidate-se diretamente às vagas de seu interesse com um clique.
+      </li>
+    </ul>
+    <h2>Para Recrutadores:</h2>
+    <ul>
+      <li>
+        <strong>Publicação de Vagas:</strong>
+        Crie e gerencie suas ofertas de emprego de maneira simples e rápida.
+      </li>
+      <li>
+        <strong>Gerenciamento de Candidaturas:</strong> Visualize e organize as
+        candidaturas recebidas para facilitar o processo de seleção
+      </li>
+      <li>
+        <strong>Conexão Direta:</strong> Entre em contato com candidatos
+        diretamente pela plataforma.
+      </li>
+    </ul>
+
+    <h3>Por Que Usar o Jobs?</h3>
+    <p>
+      Jobs foi projetado para tornar o processo de busca e recrutamento mais
+      eficiente e acessível. Com uma interface amigável, recursos robustos e
+      suporte contínuo, tanto candidatos quanto recrutadores encontrarão tudo o
+      que precisam para uma experiência positiva.
+    </p>
+
+    <ol>
+      <li>
+        <strong> Cadastro:</strong>
+        Usuários criam uma conta com informações básicas.
+      </li>
+      <li>
+        <strong>Publicação e Busca de Vagas:</strong>
+        Recrutadores publicam suas vagas, enquanto candidatos podem buscar e se
+        inscrever nas ofertas que mais lhes interessam.
+      </li>
+      <li>
+        <strong>Comunicação:</strong> Comunicação: O sistema permite que
+        recrutadores e candidatos se comuniquem diretamente, tornando o processo
+        mais ágil.
+      </li>
+    </ol>
+  </div>
+);
