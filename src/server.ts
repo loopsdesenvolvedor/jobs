17914132d90175ee444a5430dc9c63e13f539543
Elizeu Lopes <scarabellot.lopes@gmail.com>
2025-02-19 02:38:50 -0300

    feat: configure server with EJS views, static assets, and error handling

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import { join } from "path";
+
+import routes from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -7,3 +18,23 @@ app.use(
   })
 );
 
+app.use(routes);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+
+  res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App is running on port: ${port}`);
+});
