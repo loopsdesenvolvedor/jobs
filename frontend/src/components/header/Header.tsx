bf44cc2f00f815115e352c25ce782ad9e4f74c86
Elizeu Lopes <loopsdesenvolvedor@gmail.com>
2026-06-28 18:54:05 -0300

    feat: make header responsive

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -10,19 +10,19 @@ const Header = () => {
   return (
     <header className="border-b border-b-gray-200">
       <Container>
-        <div className="w-full h-12.5 flex  justify-between items-center">
+        <div className="w-full h-12.5 sm:h-13.5 flex  justify-between items-center">
           <Link href="/">
             <Image src={"./jobs.svg"} alt="Logo Jobs" width={75} height={28} />
           </Link>
 
           <div className="flex justify-end items-center gap-2.5">
             <ButtonWithIcon
-              className="w-19.5 h-7.5 text-xs flex justify-center items-center gap-2 bg-gray-200 rounded-md"
+              className="w-19.5 h-7.5 text-xs cursor-pointer flex justify-center items-center gap-2 border border-gray-200 bg-gray-200 rounded-md hover:bg-gray-300 hover:border-gray-300 duration-300"
               text="Entrar"
               icon={<UserIcon size={12} color="1C1C1C" />}
             />
             <ButtonWithIcon
-              className="w-19.5 h-7.5 text-xs uppercase flex flex-row-reverse  justify-center items-center gap-2 border border-gray-200 rounded-md"
+              className="w-7.5 sm:w-19.5 h-7.5 text-xs uppercase cursor-pointer flex flex-row-reverse  justify-center items-center gap-2 border border-gray-200 rounded-md [&>span]:hidden sm:[&>span]:flex hover:bg-gray-300 hover:border-gray-300 duration-300"
               text="Menu"
               icon={<MenuIcon size={20} color="1C1C1C" />}
             />
